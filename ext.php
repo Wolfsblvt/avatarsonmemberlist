4d8c74fe03bedf33be5c4a5b9aa0f35f3fb7b226
Wolfsblvt <clemenshusung@gmx.de>
2014-12-08 01:35:30 +0100

    v1.0.0

---
+++ ext.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * Avatars on Memberlist
+ * 
+ * @copyright (c) 2014 Wolfsblvt ( www.pinkes-forum.de )
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ * @author Clemens Husung (Wolfsblvt)
+ */
+
+namespace wolfsblvt\avatarsonmemberlist;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ext extends \phpbb\extension\base
+{
+}
